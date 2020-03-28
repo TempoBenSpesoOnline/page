ef17b1c77be539be9722b0380788af7ffdeaf842
Ruben Vergani <RubenVerg@outlook.com>
2020-03-28 13:46:35 +0100

    guide

---
+++ compile.js
@@ -0,0 +1,89 @@
+const Input = require("./locales.json"), fs = require("fs");
+let Output = "";
+
+const utils = {
+  /**
+   * @param {String} pagename
+   */
+  head: function(pagename) {
+    return `<!DOCTYPE html>
+<html>
+  <head>
+    <title>${pagename || "Home"}${Input.shared.htmlTitle}</title>
+    <meta name="viewport" content= "width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="og:title" content="${Input.shared.ogTitle}">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="${Input.shared.ogDescription}">
+    <meta property="og:url" content="/">
+    <meta property="og:image" content="/media/logo.png">
+    <meta property="og:image:alt" content="${Input.shared.ogimageAlt}">
+    <meta property="keywords" content="covid,coronavirus,tempobenspeso,tempo ben speso,casa,tempobenspesoonline,tempo ben speso online">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="shortcut icon" href="/media/logo.png" type="image/png">
+  </head>
+`},
+/**
+ * @param {String} name 
+ */
+  body: function(name) {
+    let elements = [];
+    Input.shared.list.foreach((el) => {
+      if (el.cat.indexOf(name) != -1) {elements += el}
+    });
+    elements.map((el) => {
+      return `<li id="${el.ref}"><a href="${el.href}">${el.text}</a>${el.warn ? `<span class="btn btn-outline-warning">${el.warn}</span>` : `` }${el.info ? `<span class="btn btn-outline-info">${el.info}</span>` : ``}<span class="btn invisible">Se vedi questo, allora probabilmente non riescia vedere neanche il resto del sito.</span></li>`;
+    });
+    elements.reduce((acc, val) => {
+      return acc + val + "\n";
+    }, "");
+    return `  <body>
+    <h1>${Input["#" + name]}</h1>
+    <ul>
+      ${elements}
+    </ul>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>`
+  }
+}
+
+// Root
+
+Output += utils.head(); //head
+
+Output += `  <body>
+    <h1 class="display-1">${Input["/"].title}</h1>
+    <h1 class="display-2">${Input["/"].motto}</h1>
+    <h1 class="display-3">${Input["/"].description}</h1>
+    <h2>Categorie:</h2>
+    <ul class="w-75 w-sm-50">
+      <li class="list-group-item"><a href="/museo">${Input["#museo"]}</a></li>
+      <li class="list-group-item"><a href="/divertimento">${Input["#divertimento"]}</a></li>
+      <li class="list-group-item"><a href="/rivista">${Input["#rivista"]}</a></li>
+      <li class="list-group-item"><a href="/libro">${Input["#libro"]}</a></li>
+      <li class="list-group-item"><a href="/film">${Input["#film"]}</a></li>
+      <li class="list-group-item"><a href="/audio">${Input["#audio"]}</a></li>
+      <li class="list-group-item"><a href="/green">${Input["#green"]}</li></a>
+    </ul>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>`; //body
+
+fs.writeFile("index.html", Output, (err) => {
+  if (err) throw err;
+  console.log("home done");
+});
+
+fs.writeFile("museo/index.html", utils.head("Musei") + utils.body("museo"), (err) => {
+  if (err) throw err;
+  console.log("museo done")
+})
+
+fs.writeFile("divertimento/index.html", utils.head("Divertimenti"), (err) => {
+  if (err) throw err;
+  console.log("museo done")
+})
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Come posso modificare i testi?
+É facilissimo! Nel file `locales.json`, ad ogni testo (_value_) corrisponde una _key_, cioé quella parte a **sinistra** dell'uguale.
+La key **non** va modificata, é quella che fa funzionare il sito.
+Tu puoi modificare le value.
+Le value possono non essere solo di testo, ma anche di _lista_ o di _modulo_.
+Una lista o _array_, delimitata da `[` e `]`, contiene una serie di _value_, che possono essere testi o moduli (o altre liste, ma qui non ce ne sono).
+Un modulo o _oggetto_, delimitato da `{` e `}`, é semplicemente un altro blocco di _key-value pairs_, cioé key e value uniti da `:` (come puoi vedere, tutto il documento é in realtá un modulo, delimitato da `{}`.).
+## Ok, ma cosa devo modificare?
+### `shared`
+Nella key `shared` ci sono i testi per i titoli, le preview e la lista delle cose da fare.
+|_Key_|Contenuto|Tipo|
+|---|---|---|
+|`htmlTitle`|Il titolo che vedi nella tab del browser (preceduto da Home o la _entry_ in `htmlTitles`|Testo|
+|`ogTitle`|Il titolo che vedi nella preview che c'é ad esempio su WhatsApp o Facebook|Testo|
+|`ogDescription`|La descrizione nella preview|
+|`ogimageAlt`|Il testo che c'é in preview quando l'immagine non carica o per _screen-reader_|Testo|
+|`htmlTitles`|Nella tab del browser, la parte che viene prima dell'`htmlTitle`. (se vuoi lo posso spostare a dopo, np)|Array[Testo]|
+|`list`|Una lista con tutte le cose da fare, spiegata dopo.|Array[Modulo]|
+---
+### `/`
+Dopo `shared`, c'é `/`. `/` indica le cose presenti _solo_ nella home (tipi: Testo).
+|_Key_|Contenuto|
+|---|---|
+|`title`|Il titolo in cima, io non lo cambierei tanto perché il sito alla fine é quello...|
+|`motto`|Un sottotitolo che fa parte del nome|
+|`description`|la descrizione di cos'é il sito|
+---
+### Key `#`
+Ultime, ci sono le key che iniziano con `#`. Queste rappresentano il titolo della categoria, ad esempio `#museo` é il titolo della categoria dei musei. (tipo: Testo)
+---
+### `list`: cosa c'é dentro?
+List é un Array[Modulo], quindi una lista con dentro tanti gruppi di valori (in questo caso punti dell'elenco).
+Per ogni oggetto:
+|_Key_|Contenuto|Tipo|
+|---|---|---|
+|`cat`|Categorie in cui va il punto. Se vuoi, modificale.|Array[Testo]|
+|`ref`|'Codice' assegnato al punto, accessibile a `tempobenspeso.online/<pagina>/#<ref>`|Testo
+|`text`|Descrizione del link|Testo|
+|`href`|Link del sito, non cambiarlo.|Testo|
+|`info`|(Opzionale) Eventuali informazioni su come usufruire del punto|Testo\*|
+|`warn`|(Opzionale) Eventuali informazioni **necessarie** per usufruire del punto.|Testo\*|
+
+\*: potrebbe contenere codice HTML, in qual caso **non** modificare le cose contenute tra &lt; e &gt; (oppure chiamami e basta)
+---
+## Aggiungere
+Puoi ovviamente aggiungere cose, basta aggiungere un elemento alla lista (aggiungi una virgola dopo l'ultimo, peró.)
+---
+## Quando hai modificato qualcosa, dimmelo che devo ricompilare.
+---
+## Trivia
+- Non ho la piú pallida idea di perché le key sono in inglese
+- Il tipo 'testo' si chiama in veritá Stringa
+- É stranissimo usare i nomi italiani delle cose, quindi se vuoi puoi dire string/object/array al posto di stringa/oggetto/lista.
+- Le key `href` si chiamano cosí perché in HTML un link si indica con `<a href="<link>">testo</a>`
